4ebd2bd4a0353a756c92fa5558ac06d7cfd0f295
MarcoGrossi92 <marcogrossi@live.it>
2025-04-04 11:40:03 +0200

    add subroutine to copy orion field data

--- src/fortran/lib/Lib_ORION_Data.f90
+++ src/fortran/lib/Lib_ORION_Data.f90
@@ -11,6 +11,8 @@ module Lib_ORION_data
   save
   private
 
+  public :: copyORION
+
   type :: Type_tec_Format
     character(6):: format = 'binary'  !> Binary or ascii file.
     logical     :: node   = .false.   !> Node or cell data location.
@@ -39,5 +41,61 @@ module Lib_ORION_data
     type(Type_vtk_Format) :: vtk
     type(Type_p3d_Format) :: p3d
   endtype orion_data
+
+contains
+
+   subroutine copyORION(a,b)
+    type(orion_data), intent(in)  :: a
+    type(orion_data), intent(out) :: b
+    integer :: i
+
+    ! Copy non-allocatable members
+    b%tec%format = a%tec%format
+    b%vtk%format = a%vtk%format
+    b%p3d%format = a%p3d%format
+
+    ! Handle allocation of block array
+    if (allocated(b%block)) then
+      if (size(b%block) /= size(a%block)) then
+        deallocate(b%block)
+        allocate(b%block(size(a%block)))
+      end if
+    else
+      allocate(b%block(size(a%block)))
+    end if
+
+    ! Copy each block
+    do i = 1, size(a%block)
+      b%block(i)%name = a%block(i)%name
+      b%block(i)%Ni   = a%block(i)%Ni
+      b%block(i)%Nj   = a%block(i)%Nj
+      b%block(i)%Nk   = a%block(i)%Nk
+
+      ! Handle mesh allocation
+      if (allocated(b%block(i)%mesh)) then
+        if (any(shape(b%block(i)%mesh) /= shape(a%block(i)%mesh))) then
+          deallocate(b%block(i)%mesh)
+          allocate(b%block(i)%mesh, mold=a%block(i)%mesh)
+        end if
+      else
+        allocate(b%block(i)%mesh, mold=a%block(i)%mesh)
+      end if
+
+      ! Handle vars allocation
+      if (allocated(b%block(i)%vars)) then
+        if (any(shape(b%block(i)%vars) /= shape(a%block(i)%vars))) then
+          deallocate(b%block(i)%vars)
+          allocate(b%block(i)%vars, mold=a%block(i)%vars)
+        end if
+      else
+        allocate(b%block(i)%vars, mold=a%block(i)%vars)
+      end if
+
+      ! Copy data
+      b%block(i)%mesh = a%block(i)%mesh
+      b%block(i)%vars = a%block(i)%vars
+    end do
+
+  end subroutine copyORION
 
 end module Lib_ORION_data
